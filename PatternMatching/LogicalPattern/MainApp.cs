bc42375b23eb93081db5728611d8bcc0a9d809d0
Sean Park <seanpark@hanwha.com>
2022-09-12 08:47:05 +0900

    Initial Commit

---
+++ PatternMatching/LogicalPattern/MainApp.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalPattern
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            static string GetGrade(double score) => score switch
+            {
+                < 60 => "F",
+                >= 60 and < 70 => "D",
+                >= 70 and < 80 => "C",
+                >= 80 and < 90 => "B",
+                _ => "A",
+            };
+
+        }
+    }
+}
