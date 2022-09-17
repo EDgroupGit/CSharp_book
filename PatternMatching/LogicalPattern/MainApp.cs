f66abd3f6a2b07a7ff2d484e5aca3ea9571c3f01
Sean Park <seanpark@hanwha.com>
2022-09-17 16:04:18 +0900

    Update script on 2022.9.7

--- PatternMatching/LogicalPattern/MainApp.cs
+++ PatternMatching/LogicalPattern/MainApp.cs
@@ -2,17 +2,29 @@
 {
     internal class MainApp
     {
+        class OrderItem
+        {
+            public int Amount { get; set; }
+            public int Price { get; set; }
+        }
+
+        static double GetPrice(OrderItem orderItem) => orderItem switch
+        {
+            OrderItem { Amount: 0 } or OrderItem { Price: 0 }
+                => 0.0,
+            OrderItem { Amount: >= 100 } and OrderItem { Price: >= 10_000 }
+                => orderItem.Amount * orderItem.Price * 0.8,
+            OrderItem { Amount: >= 100 }
+                => orderItem.Amount * orderItem.Price * 0.9,
+            _ => orderItem.Amount * orderItem.Price,
+        };
+
         static void Main(string[] args)
         {
-            static string GetGrade(double score) => score switch
-            {
-                < 60 => "F",
-                >= 60 and < 70 => "D",
-                >= 70 and < 80 => "C",
-                >= 80 and < 90 => "B",
-                _ => "A",
-            };
-
+            Console.WriteLine(GetPrice(new OrderItem() { Amount = 0, Price = 10_000 }));
+            Console.WriteLine(GetPrice(new OrderItem() { Amount = 100, Price = 10_000 }));
+            Console.WriteLine(GetPrice(new OrderItem() { Amount = 100, Price = 9_000 }));
+            Console.WriteLine(GetPrice(new OrderItem() { Amount = 1, Price = 1_000 }));
         }
     }
 }
---
+++ PatternMatching/ParenthesizedPattern/MainApp.cs
@@ -0,0 +1,12 @@
+﻿namespace ParenthesizedPattern
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            object age = 30;
+
+            if (age is (int and > 19))
+                Console.WriteLine("Major");
+        }
+    }
