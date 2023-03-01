21457284c03909e292da77ef74c746eb2382aed7
Sean Park <seanpark@hanwha.com>
2023-03-01 16:55:34 +0900

    Change dates

--- 03/StringFormatDatetime/MainApp.cs
+++ 03/StringFormatDatetime/MainApp.cs
@@ -8,7 +8,7 @@ namespace StringFormatDatetime
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+            DateTime dt = new DateTime(2023, 03, 04, 23, 18, 22);
                         
             WriteLine("12시간 형식: {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
             WriteLine("24시간 형식: {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
